0b32a298bfa0f7f76265132b6e0ccd3fb1cc8f3f
Jess <jessibcoden@gmail.com>
2018-04-21 13:42:47 -0500

    PirateSpeak solution in progress

--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CodingChallenge.PirateSpeak
@@ -7,7 +8,45 @@ namespace CodingChallenge.PirateSpeak
     {
         public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            var result = new List<string>();
+
+            //Solution 1:
+            var jumbleSorted = new string(jumble.ToCharArray().OrderBy(x => x).ToArray());
+
+            foreach (var word in dictionary)
+            {
+                if (word.Length == jumble.Length) 
+                {
+                    var wordSorted = word.ToCharArray().OrderBy(x => x).ToString();
+                    if (string.Equals(wordSorted, jumbleSorted, StringComparison.OrdinalIgnoreCase))
+                        result.Add(word);
+                }
+
+            //Solution 2:
+            //var jumbleChar = jumble.ToCharArray();
+
+            //foreach (var word in dictionary)
+            //{
+                //if (word.Length == jumble.Length)
+                //{
+                //    var wordChar = word.ToCharArray().ToList();
+                //    var numberOfMatches = 0;
+
+                //    foreach (var character in jumbleChar)
+                //    {
+                //        if (wordChar.Contains(character))
+                //        {
+                //            wordChar.Remove(character);
+                //            numberOfMatches++;
+                //        }
+                //    }
+
+                //    if (numberOfMatches == jumble.Length)
+                //    result.Add(word);
+                //}
+            }
+
+            return result.ToArray();
         }
     }
 }
